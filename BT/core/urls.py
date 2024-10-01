3ba9741eee8133ab9fe6486701d8607298d9c264
he-jaramillo <HE.JARAMILLO@duocuc.cl>
2024-10-01 10:07:58 -0300

    Agrega Servicios
    Se agrega pagina de servicios y fotografias (no funcional aun.)

--- BT/core/urls.py
+++ BT/core/urls.py
@@ -6,18 +6,15 @@ from . import views
 urlpatterns = [
     # INICIO
     path("", views.home, name="home"),
-    path("logout/", views.exit, name="exit"),    
-    path('registration/register', views.register, name="register"),
-
-    path('registration/login', views.login, name="login"),
-
+    path("logout/", views.exit, name="exit"),
+    path("registration/register", views.register, name="register"),
+    path("registration/login", views.login, name="login"),
     # PAGINAS
-   
-    path('paginas/perfil', views.perfil, name="perfil"),
-    path('paginas/editarPerfil', views.editarPerfil, name="editarPerfil"),
-
-    path('paginas/usuarios', views.usuarios, name="usuarios"),    
-    path('paginas/agregarUsuario', views.agregarUsuario, name="agregarUsuario"),
-    path('paginas/eliminarUsuario', views.eliminarUsuario, name="eliminarUsuario"),
-    path('paginas/editarUsuario', views.editarUsuario, name="editarUsuario"),
+    path("paginas/perfil", views.perfil, name="perfil"),
+    path("paginas/editarPerfil", views.editarPerfil, name="editarPerfil"),
+    path("paginas/usuarios", views.usuarios, name="usuarios"),
+    path("paginas/agregarUsuario", views.agregarUsuario, name="agregarUsuario"),
+    path("paginas/eliminarUsuario", views.eliminarUsuario, name="eliminarUsuario"),
+    path("paginas/editarUsuario", views.editarUsuario, name="editarUsuario"),
+    path("paginas/servicios", views.servicios, name="servicios"),
 ]
--- BT/core/views.py
+++ BT/core/views.py
@@ -2,7 +2,7 @@ import os
 import requests
 
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required 
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.contrib.auth import authenticate, login
 
@@ -11,119 +11,122 @@ from .forms import *
 
 # HOME
 
+
 def home(request):
     print(request.session.session_key)
 
-    return render(request, 'base/home.html', {'home': home})
+    return render(request, "base/home.html", {"home": home})
 
 
 def login(request):
     print(request.session.session_key)
 
-    return render(request, 'registration/login.html', {'login': login})
+    return render(request, "registration/login.html", {"login": login})
+
+
 # PERFILES
 
-#REGISTRARSE
-def register(request):
-    data = {
-        'form': CustomUserCreationForm()
-    }
 
-    if request.method == 'POST':
-        user_creation_form = CustomUserCreationForm(data = request.POST)
+# REGISTRARSE
+def register(request):
+    data = {"form": CustomUserCreationForm()}
+
+    if request.method == "POST":
+        user_creation_form = CustomUserCreationForm(data=request.POST)
 
         if user_creation_form.is_valid():
             user_creation_form.save()
 
-            user = authenticate(username=user_creation_form.cleaned_data['username'], password=user_creation_form.cleaned_data['password1'] )
-            login (request, user)
+            user = authenticate(
+                username=user_creation_form.cleaned_data["username"],
+                password=user_creation_form.cleaned_data["password1"],
+            )
+            login(request, user)
 
-            return redirect('home') 
+            return redirect("home")
 
-    return render(request, 'registration/register.html', data)
+    return render(request, "registration/register.html", data)
 
-#VER PERFIL PROPIO
+
+# VER PERFIL PROPIO
 @login_required
 def perfil(request):
     form = editarPerfilForm()
-    context = {
-        
-        'form':form
-    }
-    return render(request, 'registration/perfil.html', context)
+    context = {"form": form}
+    return render(request, "registration/perfil.html", context)
 
-#EDITAR PERFIL PROPIO
+
+# EDITAR PERFIL PROPIO
 @login_required
 def editarPerfil(request):
-    if request.method == 'POST':
-        user = get_object_or_404(User, pk=request.POST.get('id_perfil_editar'))
-        form = editarPerfilForm(data=request.POST, files=request.FILES, instance=request.user)
+    if request.method == "POST":
+        user = get_object_or_404(User, pk=request.POST.get("id_perfil_editar"))
+        form = editarPerfilForm(
+            data=request.POST, files=request.FILES, instance=request.user
+        )
         if form.is_valid():
             form.save()
-        return redirect('perfil')
+        return redirect("perfil")
     else:
         form = editarPerfilForm()
-        context = {
-            'form': form
-        }
-    return render(request, 'paginas/perfil.html', context)
+        context = {"form": form}
+    return render(request, "paginas/perfil.html", context)
 
-#EDITAR USUARIO DESDE ADMINISTRADOR GENERAL
+
+# EDITAR USUARIO DESDE ADMINISTRADOR GENERAL
 @login_required
 def editarUsuario(request):
-    if request.method == 'POST':
-        user = get_object_or_404(User, pk=request.POST.get('id_usuario_editar'))
-        form_editar = editarUsuarioForm(data=request.POST, files=request.FILES, instance=user)
+    if request.method == "POST":
+        user = get_object_or_404(User, pk=request.POST.get("id_usuario_editar"))
+        form_editar = editarUsuarioForm(
+            data=request.POST, files=request.FILES, instance=user
+        )
         if form_editar.is_valid():
             form_editar.save()
-           
-        return redirect('usuarios')
+
+        return redirect("usuarios")
     else:
         form_editar = editarUsuarioForm()
-        context = {
-            'form_editar': form_editar
-        }
-    return render(request, 'paginas/usuarios.html', context)
+        context = {"form_editar": form_editar}
+    return render(request, "paginas/usuarios.html", context)
 
-#FUNCIÓN PARA SALIR DE LA SESIÓN DEL USUARIO
+
+# FUNCIÓN PARA SALIR DE LA SESIÓN DEL USUARIO
 def exit(request):
     logout(request)
-    return redirect('home')
+    return redirect("home")
 
-#FUNCIÓN PARA EL ADMINISTRADOR GENERAL PARA LA GESTIÓN DE USUARIOS
+
+# FUNCIÓN PARA EL ADMINISTRADOR GENERAL PARA LA GESTIÓN DE USUARIOS
 @login_required
 def usuarios(request):
     users = User.objects.all()
     form_editar = editarUsuarioForm()
-    context = {
-        'users': users,
-        'form_editar':form_editar
-    }
-  
-    return render(request, 'registration/usuarios.html', context)
+    context = {"users": users, "form_editar": form_editar}
+
+    return render(request, "registration/usuarios.html", context)
 
 
-#FUNCIÓN PARA EL ADMINISTRADOR GENERAL PARA  AGREGAR UN USUARIO
+# FUNCIÓN PARA EL ADMINISTRADOR GENERAL PARA  AGREGAR UN USUARIO
 @login_required
 def agregarUsuario(request):
-    
-    if request.method == 'POST':
-        form = editarUsuarioForm(data = request.POST, files = request.FILES)
+
+    if request.method == "POST":
+        form = editarUsuarioForm(data=request.POST, files=request.FILES)
         if form.is_valid():
             form.save()
-        return redirect ('usuarios')
+        return redirect("usuarios")
     else:
         form = editarUsuarioForm()
-        context = {
-            'form': form
-        }
-    return render(request, 'registration/agregarUsuario.html', context)
+        context = {"form": form}
+    return render(request, "registration/agregarUsuario.html", context)
 
-#FUNCIÓN PARA EL ADMINISTRADOR GENERAL PARA ELIMINAR UN USUARIO
+
+# FUNCIÓN PARA EL ADMINISTRADOR GENERAL PARA ELIMINAR UN USUARIO
 @login_required
 def eliminarUsuario(request):
     if request.POST:
-        users = User.objects.get(pk=request.POST.get('id_usuario_eliminar'))
-        users.delete()    
-  
-    return redirect('usuarios')
+        users = User.objects.get(pk=request.POST.get("id_usuario_eliminar"))
+        users.delete()
+
+    return redirect("usuarios")
